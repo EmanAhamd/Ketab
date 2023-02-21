73a9d94c6e5cfc9369d7c09235590a46a13f757f
Eman Ahmed <emanamra1999@gmail.com>
2023-02-21 10:10:04 +0200

    Upgrade som arche

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { RegisterComponent } from './Components/Auth/register/register.component';
-import { ArabicBooksComponent } from './Components/User/arabic-books/arabic-books.component';
-import { ChildrenBooksComponent } from './Components/User/children-books/children-books.component';
-import { EnglishBooksComponent } from './Components/User/english-books/english-books.component';
-import { HomeComponent } from './Components/User/home/home.component';
-import { NewArrivalsComponent } from './Components/User/new-arrivals/new-arrivals.component';
+import { ArabicBooksComponent } from './Components/User/Arabic-Books/arabic-books/arabic-books.component';
+import { ChildrenBooksComponent } from './Components/User/Children-Books/children-books/children-books.component';
+import { EnglishBooksComponent } from './Components/User/English-Books/english-books/english-books.component';
+import { HomeComponent } from './Components/User/Home/home/home.component';
+import { NewArrivalsComponent } from './Components/User/New-Arrivals/new-arrivals/new-arrivals.component';
+
 import { NotFoundPageComponent } from './Components/User/not-found-page/not-found-page.component';
 
 
