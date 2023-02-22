6ad5c70aa797c9a84912ea27a95e7f5e03b3f3c7
Eman Ahmed <emanamra1999@gmail.com>
2023-02-22 20:35:36 +0200

    static array in book service for test

--- src/app/Core/Services/book.service.ts
+++ src/app/Core/Services/book.service.ts
@@ -7,6 +7,24 @@ import { Observable, throwError } from 'rxjs';
 })
 export class BookService {
 
+  EnglishBooks:any[] =[
+    {id:1, title: 'Alchemy Of Souls', category: 'Fiction',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+    {id:2, title: 'Racet Boyes', category: 'Health',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:3, title: 'Moon Loveres', category: 'History',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:4, title: 'Dali And Cocky Prince', category: 'Art',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:5, title: 'Little Women', category: 'Literature', price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:6, title: "I'm Not Robot", category: 'Science',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:7, title: 'Train To Busan', category: 'Travel',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+    {id:8, title: 'Are You Human Too?', category: 'Technology',price:'15$', img: 'https://m.media-amazon.com/images/M/MV5BZGU1ODJmM2UtNDk0OS00MWIyLWJlNjItMDU3MTU2ZmYxNTM4XkEyXkFqcGdeQXVyMTQyMTMwOTk0._V1_.jpg'},
+
+  ]
+
   constructor(private http:HttpClient) { }
 
   getNewArrivalBooks():Observable<any>{
@@ -17,5 +35,6 @@ export class BookService {
     return this.http.get(`https://api.itbook.store/1.0/books/${isbn13}`);
   }
 
+  
 
 }
