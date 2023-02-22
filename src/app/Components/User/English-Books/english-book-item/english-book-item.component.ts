412ed1c415e4b96421156977820faac1435e1370
Eman Ahmed <emanamra1999@gmail.com>
2023-02-22 17:56:41 +0200

    add search pipe and 2 comp

---
+++ src/app/Components/User/English-Books/english-book-item/english-book-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-english-book-item',
+  templateUrl: './english-book-item.component.html',
+  styleUrls: ['./english-book-item.component.css']
+})
+export class EnglishBookItemComponent {
+
+}
