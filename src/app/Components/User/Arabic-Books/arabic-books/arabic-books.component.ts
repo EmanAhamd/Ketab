02e3097da8d92c90b44c390d194dcb9a4ac905fc
Eman Ahmed <emanamra1999@gmail.com>
2023-02-23 14:11:16 +0200

    add english books

--- src/app/Components/User/Arabic-Books/arabic-books/arabic-books.component.ts
+++ src/app/Components/User/Arabic-Books/arabic-books/arabic-books.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BookService } from 'src/app/Core/Services/book.service';
 
 @Component({
   selector: 'app-arabic-books',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./arabic-books.component.css']
 })
 export class ArabicBooksComponent {
+  term: string = '';
+  FilteriedBooks: any[] = [];
+  chips: any[] = ["All"];
+  constructor(private myBookService: BookService) {
+    this.ArabicBooks.forEach(item => {
+      if (!this.chips.includes(item.category)) {
+        this.chips.push(item.category)
+      }
+    });
+    this.FilteriedBooks = this.ArabicBooks
+  }
+  ArabicBooks: any[] = this.myBookService.ArabicBooks;
 
+  filterByCategory(chip: any) {
+    if (chip === "All") {
+      this.FilteriedBooks = this.ArabicBooks;
+    } else
+      this.FilteriedBooks = this.ArabicBooks.filter(item => item.category.toLocaleLowerCase().includes(chip.toLocaleLowerCase()));
+  }
 }
