b50119bb3014e5b2ce0b601849067a1426cb71ca
Eman Ahmed <emanamra1999@gmail.com>
2023-02-27 15:24:37 +0200

    login&register design

--- src/app/Components/User/book-details/book-details.component.ts
+++ src/app/Components/User/book-details/book-details.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { BookService } from 'src/app/Core/Services/book.service';
-ActivatedRoute
-BookService
+
 
 @Component({
   selector: 'app-book-details',
@@ -12,15 +11,24 @@ BookService
 export class BookDetailsComponent {
   Id:any;
   bookDetails:any;
+  selectedQuantity:any;
+  
 
   constructor(private bookService:BookService, private Route:ActivatedRoute ){
     this.Id = Route.snapshot.paramMap.get('id');
     bookService.getBookDetails(this.Id).subscribe((res) =>{
       this.bookDetails = res;
-      
       console.log("id",this.Id);
-      
-      
     })
+    
+    
   }
+  getQuantityValue(value:any){
+    console.log("value",value);
+    // this.bookService.testQuantity = value;
+    // localStorage.setItem("booksQuantityValue",value)
+  }
+
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -81,7 +82,8 @@ import { ChildrenEnglishBooksComponent } from './Components/User/Children-Books/
     HttpClientModule,
     BrowserAnimationsModule,
     CarouselModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
