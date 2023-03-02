01fd34c7be372d7cb40527c707f839d30b189baa
Eman Ahmed <emanamra1999@gmail.com>
2023-03-02 15:50:10 +0200

    app-comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+
+
 import { AdminLayoutComponent } from './Components/Admin/admin-layout/admin-layout.component';
 import { CrudAuthorsComponent } from './Components/Admin/crud-authors/crud-authors.component';
 import { CrudBooksComponent } from './Components/Admin/crud-books/crud-books.component';
 import { AdminHomeComponent } from './Components/Admin/Dashboard/admin-home/admin-home.component';
+
+
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { RegisterComponent } from './Components/Auth/register/register.component';
 import { ArabicBooksComponent } from './Components/User/Arabic-Books/arabic-books/arabic-books.component';
@@ -18,6 +23,10 @@ import { NewArrivalsComponent } from './Components/User/New-Arrivals/new-arrival
 import { NotFoundPageComponent } from './Components/User/not-found-page/not-found-page.component';
 import { AuthGuard } from './Core/guards/auth.guard';
 import { HasRoleGuard } from './Core/guards/has-role.guard';
+
+
+
+
 
 
 
@@ -45,6 +54,10 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: NotFoundPageComponent },
+
+  
+
+
 ];
 
 @NgModule({
